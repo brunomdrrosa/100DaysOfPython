61b855f842c2fffe9176dfb0fb153d9b804960ea
Bruno Machado <brunomdr46@gmail.com>
2021-05-29 21:18:53 -0300

    Random

--- Day4/pedraPapelTesoura.py
+++ Day4/pedraPapelTesoura.py
@@ -29,15 +29,23 @@ tesoura = '''
 escolha = input("O que você quer escolher? Digite 0 para Pedra, 1 para Papel ou 2 para Tesoura.\n")
 
 if escolha == "0":
+    print("Você escolheu pedra")
     print(pedra)
 elif escolha == "1":
+    print("Você escolheu papel")
     print(papel)
 elif escolha == "2":
+    print("Você escolheu tesoura")
     print(tesoura)
 
 computador = [pedra, papel, tesoura]
 escolhaPC = random.choice(computador)
-print("O computador escolheu:")
+if escolhaPC == pedra:
+    print("O computador escolheu pedra")
+elif escolhaPC == papel:
+    print("O computador escolheu papel")
+elif escolhaPC == tesoura:
+    print("O computador escolheu tesoura")
 print(escolhaPC)
 
 # Computador escolheu pedra.
