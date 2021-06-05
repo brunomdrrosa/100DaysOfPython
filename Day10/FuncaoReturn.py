c77721f24bcf9f0caf0e4017d8ee41541b980b1e
Bruno Machado <brunomdr46@gmail.com>
2021-06-05 15:24:30 -0300

    Funções Output

---
+++ Day10/FuncaoReturn.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+  formated_f_name = (f_name.title())
+  formated_l_name = (l_name.title())
+  return f"{formated_f_name} {formated_l_name}"
+
+print(format_name("bruno", "MACHADO"))
