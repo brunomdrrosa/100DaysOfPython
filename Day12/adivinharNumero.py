940c51da72931182b2165e195cab220a3614a5e1
Bruno Machado <brunomdr46@gmail.com>
2021-06-08 20:00:45 -0300

    Adivinhar número

--- Day12/adivinharNumero.py
+++ Day12/adivinharNumero.py
@@ -1,83 +1,121 @@
-#Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
+# Importar a logo, módulo aleatório e opção de limpar o console
 from arte import logo
 import random
+import os
 
 # Criar um número aleatório de 1 a 100
 numero = random.randint(1,100)
 
-# Mensagens de início do jogo
+# Reiniciar o jogo
+def reiniciar():
+    restart = input("Você deseja jogar novamente? Digite 'S' ou 'N': ").lower()
+    if restart == 's':
+        cls = lambda: os.system('cls')
+        cls()
+        jogar()
+    elif restart == 'n':
+        exit()
+
+# Dificuldade fácil
+def facil():
+    tentativas = 10
+    print(f"Você tem {tentativas} tentativas para adivinhar o número")
+    if tentativas != 0:
+        palpite = int(input("Faça um palpite: "))
+    if palpite == numero:
+        print(f"A chance era de 1% e você conseguiu! O número era {numero}")
+        print()
+        reiniciar()
+
+    while tentativas != 0:
+        if palpite > numero:
+            tentativas -= 1
+            if tentativas == 0:
+                print()
+                print(f"Você perdeu, o numero era {numero}.")
+                reiniciar()
+            print("Número muito alto.")
+            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
+            print()
+            palpite = int(input("Faça um palpite: "))
+            if palpite == numero:
+                print(f"Você conseguiu! O número era {numero}")
+                print()
+                reiniciar()
+            if tentativas == 0:
+                reiniciar()
+
+        elif palpite < numero:
+            tentativas -= 1
+            if tentativas == 0:
+                print()
+                print(f"Você perdeu, o numero era {numero}.")
+                reiniciar()
+            print("Número muito baixo.")
+            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
+            print()
+            palpite = int(input("Faça um palpite: "))
+            if palpite == numero:
+                print(f"Você conseguiu! O número era {numero}")
+                print()
+                reiniciar()
+            if tentativas == 0:
+                reiniciar()
+
+# Dificuldade difícil
+def dificil():
+    tentativas = 5
+    print(f"Você tem {tentativas} tentativas para adivinhar o número")
+    if tentativas != 0:
+        palpite = int(input("Faça um palpite: "))
+    if palpite == numero:
+        print(f"A chance era de 1% e você conseguiu! O número era {numero}")
+        print()
+        reiniciar()
+
+    while tentativas != 0:
+        if palpite > numero:
+            tentativas -= 1
+            if tentativas == 0:
+                print()
+                print(f"Você perdeu, o numero era {numero}.")
+                reiniciar()
+            print("Número muito alto.")
+            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
+            print()
+            palpite = int(input("Faça um palpite: "))
+            if palpite == numero:
+                print(f"Você conseguiu! O número era {numero}")
+                print()
+                reiniciar()
+            if tentativas == 0:
+                reiniciar()
+
+        elif palpite < numero:
+            tentativas -= 1
+            if tentativas == 0:
+                print()
+                print(f"Você perdeu, o numero era {numero}.")
+                reiniciar()
+            print("Número muito baixo.")
+            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
+            print()
+            palpite = int(input("Faça um palpite: "))
+            if palpite == numero:
+                print(f"Você conseguiu! O número era {numero}")
+                print()
+                reiniciar()
+            if tentativas == 0:
+                reiniciar()
+
 def jogar():
     print(logo)
     print("Bem-vindo ao jogo 'Adivinhe o número'")
     print("Eu estou pensando num número entre 1 e 100.")
     dificuldade = input("Escolha uma dificuldade. Digite 'fácil' ou 'difícil': ").lower()
-    
-# Dificuldade fácil
-    if dificuldade == "facil" or "fácil":
-        tentativas = 10
-        print(f"Você tem {tentativas} tentativas para adivinhar o número")
-        palpite = int(input("Faça um palpite: "))
-        if palpite == numero:
-            print(f"Você conseguiu! O número era {numero}")
-        while palpite > numero:
-            tentativas -= 1
-            print("Número muito alto.")
-            print("Faça um palpite novamente.")
-            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
-            palpite = int(input("Faça um palpite: "))
-            if palpite == numero:
-                print(f"Você conseguiu! O número era {numero}")
-            if tentativas == 1:
-                print("Você gastou todas suas tentativas, você perdeu.")
-                exit()
-        while palpite < numero:
-            tentativas -= 1
-            print("Número muito baixo.")
-            print("Faça um palpite novamente.")
-            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
-            palpite = int(input("Faça um palpite: "))
-            if palpite == numero:
-                print(f"Você conseguiu! O número era {numero}")
-            if tentativas == 1:
-                print("Você gastou todas suas tentativas, você perdeu.")
-
-# Dificuldade difícil
-    elif dificuldade == "dificil" or "difícil":
-        print("DIFICULDADE DIFICIL")
-        tentativas = 5
-        print(f"Você tem {tentativas} tentativas para adivinhar o número")
-        palpite = int(input("Faça um palpite: "))
-        if palpite == numero:
-            print(f"Você conseguiu! O número era {numero}")
-        while palpite > numero:
-            tentativas -= 1
-            print("Número muito alto.")
-            print("Faça um palpite novamente.")
-            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
-            palpite = int(input("Faça um palpite: "))
-            if palpite == numero:
-                print(f"Você conseguiu! O número era {numero}")
-            if tentativas == 1:
-                print("Você gastou todas suas tentativas, você perdeu.")
-                exit()
-        while palpite < numero:
-            tentativas -= 1
-            print("Número muito baixo.")
-            print("Faça um palpite novamente.")
-            print(f"Você tem {tentativas} tentativas restantes para adivinhar o número.")
-            palpite = int(input("Faça um palpite: "))
-            if palpite == numero:
-                print(f"Você conseguiu! O número era {numero}")
-            if tentativas == 1:
-                print("Você gastou todas suas tentativas, você perdeu.")
+    if dificuldade == "facil" or dificuldade == "fácil":
+        facil()
+    elif dificuldade == "dificil" or dificuldade == "difícil":
+        dificil()
 
 jogar()
-
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
