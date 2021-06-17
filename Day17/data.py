a96063182aca54279d15b0398bdfb0b7507e1423
Bruno Machado <brunomdr46@gmail.com>
2021-06-17 17:54:47 -0300

    Quiz Usando POO

---
+++ Day17/data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "O sangue de uma lesma é verde.", "answer": "Verdadeiro"},
+    {"text": "O animal mais barulhento é o elefante africano.", "answer": "Falso"},
+    {"text": "Aproximadamente um quarto dos ossos humanos estão nos pés.", "answer": "Verdadeiro"},
+    {"text": "A área total de superfície de um pulmão humano é do tamanho de um campo de futebol.", "answer": "Verdadeiro"},
+    {"text": "Em Virgínia Ocidental - EUA, se você acidentalmente atropelar um animal com seu carro, pode levá-lo para comer em casa.", "answer": "Verdadeiro"},
+    {"text": "Em Londres - Reino Unido, se acontecer de você morrer na Casa do Parlamento, você tem direito a um funeral de estado.", "answer": "Falso"},
+    {"text": "É ilegal fazer xixi no oceano em Portugal.", "answer": "Verdadeiro"},
+    {"text": "Você pode conduzir uma vaca escada abaixo, mas não escada acima.", "answer": "Falso"},
+    {"text": "O Google era originalmente chamado de 'Backrub'.", "answer": "Verdadeiro"},
+    {"text": "O nome de solteira da mãe de Buzz Aldrin era 'Moon'.", "answer": "Verdadeiro"},
+    {"text": "Nenhum pedaço de papel quadrado seco pode ser dobrado ao meio mais de 7 vezes.", "answer": "Falso"},
+    {"text": "Algumas gramas de chocolate podem matar um cachorro pequeno.", "answer": "Verdadeiro"}
+]
